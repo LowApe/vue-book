7509e83c05fbfd920f4b55c3786560b28a01407f
didiaoyuan <didiaoyuan@gmail.com>
2019-05-15 18:00:58 +0800

    init and use vue-router build entry

---
+++ src/Explorer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="explorer">探索</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+// 引入四个页面
+import Home from '@/Home.vue'
+import Explorer from '@/Explorer.vue'
+import Cart from '@/Cart.vue'
+import Me from '@/Me.vue'
+// 使用路由实例插件
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+   {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/explorer',
+      name: 'Explorer',
+      component: Explorer
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
+      path: '/me',
+      name: 'Me',
+      component: Me
+    }
+  ]
+})
