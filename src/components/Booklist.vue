25464c393b02b1ebb4fdedc168e3e2da0ae62dba
didiaoyuan <didiaoyuan@gmail.com>
2019-05-25 21:53:03 +0800

    创建首页的组件

---
+++ src/components/Booklist.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="me">我</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,28 @@
 <template>
-    <div id="home">
-        首页
-        <ul>
-            <router-link :to="{name : 'BookDetails',params:{id:1}}">
-                <div>图书</div>
-            </router-link>
-        </ul>
+  <div id="home">
+    首页
+    <ul>
+      <router-link :to="{name : 'BookDetails',params:{id:1}}">
+        <div>图书</div>
+      </router-link>
+    </ul>
+    <div class="section">
+      <div>
+        <!-- 热门推荐 -->
+        <!-- 快讯 -->
+      </div>
     </div>
+    <div class="section">
+      <div>
+        <!-- 新书上架 -->
+      </div>
+    </div>
+    <div class="section">
+      <div>
+        <!-- 编辑推荐 -->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,5 +30,4 @@ export default {}
 </script>
 
 <style>
-
 </style>
